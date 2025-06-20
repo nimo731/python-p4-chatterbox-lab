2e53076b69f93502b84190e6c1aeaec418ae1170
nimo731 <patience.karanja@student.moringaschool.com>
2025-06-20 16:17:26 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 from models import db, Message
 
@@ -14,13 +15,49 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods=['GET'])
+def get_messages():
+    messages = Message.query.order_by(Message.created_at.asc()).all()
+    return jsonify([message.to_dict() for message in messages]), 200
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
     return ''
+
+@app.route('/')
+def home():
+    return "Chatterbox API is running!"
+
+@app.route('/messages', methods=['POST'])
+def create_message():
+    data = request.get_json()
+    message = Message(
+        body=data.get('body'),
+        username=data.get('username')
+    )
+    db.session.add(message)
+    db.session.commit()
+    return jsonify(message.to_dict()), 201
+
+@app.route('/messages/<int:id>', methods=['PATCH'])
+def update_message(id):
+    message = db.session.get(Message, id)
+    if not message:
+        abort(404)
+    data = request.get_json()
+    if 'body' in data:
+        message.body = data['body']
+    db.session.commit()
+    return jsonify(message.to_dict()), 200
+
+@app.route('/messages/<int:id>', methods=['DELETE'])
+def delete_message(id):
+    message = db.session.get(Message, id)
+    if not message:
+        abort(404)
+    db.session.delete(message)
+    db.session.commit()
+    return '', 204
 
 if __name__ == '__main__':
     app.run(port=5555)
